09a4e2de3a5f9e8988fd6fc3b82e8c3553a6d3de
Devendra Kumar Patel <92505396+devendra1707@users.noreply.github.com>
2023-02-24 16:45:41 +0530

    Java Script Program

---
+++ JS 11am/myfuns.js
@@ -0,0 +1,11 @@
+/* example on external functions */
+function fun1()
+{
+	document.write("hi, i am fun1");
+}
+function fun2()
+{
+	document.write("hi, i am fun2");
+}
+
+//Note: save this file with myfuns.js
