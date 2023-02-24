1e992fd3fd564467ae32e2486ed9edcf8a3c7642
Devendra Kumar Patel <92505396+devendra1707@users.noreply.github.com>
2023-02-24 16:34:15 +0530

    HTML Program

---
+++ html11am/scripts.js
@@ -0,0 +1,12 @@
+/* example on creating exrternal script */
+function fun1()
+{
+  document.write("hi, i am fun1");
+}
+
+function fun2()
+{
+  document.write("hi, i am fun2");
+}
+
+//Note: save this file with a name "scripts.js"
